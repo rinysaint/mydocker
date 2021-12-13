cb7c402c27f837e9be181fcfb1c020270a4fb999
rinysaint <rinysaint@163.com>
2021-12-13 15:44:43 +0800

    20211213

---
+++ start.sh
@@ -0,0 +1,1 @@
+docker run -d --restart=unless-stopped --name=kuboard -p 1080:80/tcp -p 10081:10081/udp -p 10081:10081/tcp -e KUBOARD_ENDPOINT="http://192.168.99.100" -e KUBOARD_AGENT_SERVER_UDP_PORT="10081" -e KUBOARD_AGENT_SERVER_TCP_PORT="10081" swr.cn-east-2.myhuaweicloud.com/kuboard/kuboard:v3.3.0.3
